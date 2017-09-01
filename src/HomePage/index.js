adc443cd995be09b579a855ee97f6bafaf7e4799
miitri raatikainen <miitri.raatikainen@siili.com>
2017-09-01 11:22:08 +0300

    App layout and basic routing.

---
+++ src/HomePage/index.js
@@ -0,0 +1,3 @@
+import component from './HomePage';
+
+export default component;
---
+++ src/Layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const Header = () => (
+  <div className="Header">
+    <img src={logo} className="Header__logo" alt="logo" />
+    <h1>Welcome to React</h1>
+  </div>
+);
+
+export default Header;
