619cf5dbea30d70618913a6214b3a20e51308069
Liisa Lado-Villar <liisa.lado-villar@espoo.fi>
2017-09-05 13:03:17 +0300

    Add project button at home screen

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'redux-little-router';
+import t from '../../locale';
+import { PROJECT } from '../../constants/routes';
+import Button from '../common/Button';
 
 /**
  * Landing page
  */
 const HomePage = () => (
-  <div />
+  <div>
+    <Button typeClass="primary">
+      <Link href={PROJECT}>{t('button.add_project')}</Link>
+    </Button>
+  </div>
 );
 
 export default HomePage;
