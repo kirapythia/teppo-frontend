7031180b592c29e9412720417f569a8b8637bcf6
miitri raatikainen <miitri.raatikainen@siili.com>
2017-09-05 13:47:57 +0300

    Add project page, stub for route validation.

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Link } from 'redux-little-router';
 
 import { HOME } from '../../constants/routes';
+import t from '../../locale';
+import Button from '../common/Button';
 
 /**
  * Page for project details
  */
 const ProjectPage = () => (
   <div>
-    <h2>Project</h2>
-    <Link href={HOME}>&lt; home page</Link>
+    <h2>{ t('button.add_project') }</h2>
+    <Button typeClass="primary">
+      <Link href={HOME}>{t('button.cancel')}</Link>
+    </Button>
   </div>
 );
 
