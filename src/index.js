794562ab290362cf0171b5802391ce56b573a4e4
miitri raatikainen <miitri.raatikainen@siili.com>
2017-09-06 14:35:27 +0300

    Add font-awsome to the project. Improve layout and form styles.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import 'milligram/dist/milligram.min.css';
 import 'normalize.css/normalize.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import App from './components/App';
 
 import './index.css';
