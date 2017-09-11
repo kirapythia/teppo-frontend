4a362cdc12aa99d5734dff727dd2363aeb280d06
miitri raatikainen <miitri.raatikainen@siili.com>
2017-09-11 14:02:37 +0300

    Remove string to number normalizer because it's too error prone.

--- src/forms/normalizers.js
+++ src/forms/normalizers.js
@@ -1,14 +1,2 @@
-import { isNil } from '../utils';
-
 export const stringToList = value => (value ? value.replace(/,\s/g, ',').split(/,\s?/) : []);
 export const listToString = value => (value ? value.join(', ') : '');
-/**
- * Convert string to number
- * @param {string} value
- * @return {number}
- */
-export const toNumber = (value) => {
-  const converted = Number(value);
-  if (isNaN(converted)) return value;
-  return isNil(value) || !value.length ? null : converted;
-};
