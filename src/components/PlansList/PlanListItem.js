8bab32e9e8d38a22452f535620de204250910195
miitri raatikainen <miitri.raatikainen@siili.com>
2017-10-25 11:03:25 +0300

    Display each file url in plan list item

--- src/components/PlansList/PlanListItem.js
+++ src/components/PlansList/PlanListItem.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import * as R from 'ramda';
 import { Link } from 'redux-little-router';
 import { formIdentifier } from './model';
 import PLAN_STATUS from '../../constants/plan-status';
-import { formPlanUrl, parseFileNameFromURL, versionToCharacter } from '../../utils';
+import { getPlanFileUrls, formPlanUrl, parseFileNameFromURL, versionToCharacter } from '../../utils';
+
+/**
+ * Form link elements for each file url
+ * @private
+ * @param {object}
+ * @return {React.Element[]|string}
+ */
+const formPlanFileUrls = R.pipe(
+  getPlanFileUrls,
+  // eslint-disable-next-line react/no-array-index-key
+  R.map((url, idx) => (<a key={`${url}_${idx}`} href={url} className="PlanListItem__fileUrl" target="_blank">{parseFileNameFromURL(url)}</a>)),
+  R.defaultTo('-'),
+);
 
 /**
  * Component for displaying project's plans as a list
@@ -24,11 +38,7 @@ const PlanListItem = ({ plan = {}, project = {}, readOnly }) => (
         {formIdentifier(plan)}
       </Link>
     </div>
-    <div className="five columns">{(plan.url
-      ? <a href={plan.url} target="_blank">{parseFileNameFromURL(plan.url)}</a>
-      : '-'
-    )}
-    </div>
+    <div className="five columns">{formPlanFileUrls(plan)}</div>
     <div className="one columns">{versionToCharacter(plan.version)}</div>
     <div className="two columns text-right">
       {readOnly
