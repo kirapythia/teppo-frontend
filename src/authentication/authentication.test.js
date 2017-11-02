527d9dd83616c7c3f98142e22ee9f90f6c456164
miitri raatikainen <miitri.raatikainen@siili.com>
2017-11-02 13:37:21 +0200

    Tests for route authorization

--- src/authentication/authentication.test.js
+++ src/authentication/authentication.test.js
@@ -1,5 +1,33 @@
+import * as Routes from '../constants/routes';
 import authenticate from './index';
 
-it('should pass validation', () => {
-  expect(authenticate()).toEqual(true);
+// mock the authorization rules to prevent tests from breaking when the rules change
+jest.mock('../constants/user_authorization', () => ({ '/projects': ['ADMIN'] }));
+
+it('should pass validation if user is not defined and route is HOME', () => {
+  expect(authenticate({}, { payload: { route: Routes.HOME } })).toEqual(true);
+});
+
+it('should pass validation if user is not defined and route is NOT_FOUND', () => {
+  expect(authenticate({}, { payload: { route: Routes.NOT_FOUND_PAGE } })).toEqual(true);
+});
+
+it('should pass validation if user\'s role is found in authorization rules list', () => {
+  expect(authenticate({ user: 'User Name', role: 'ADMIN' }, { payload: { route: '/projects' } })).toEqual(true);
+});
+
+it('should pass validation if authorization rules are not found for current route', () => {
+  expect(authenticate({ user: 'User Name', role: 'ADMIN' }, { payload: { route: 'mock_route' } })).toEqual(true);
+});
+
+it('should not pass validation if user is not defined and route is neither HOME nor NOT_FOUND', () => {
+  expect(authenticate({}, { payload: { route: '/projects' } })).toEqual(false);
+});
+
+it('should not pass validation if user is not defined and route is neither HOME nor NOT_FOUND', () => {
+  expect(authenticate({}, { payload: { route: '/projects' } })).toEqual(false);
+});
+
+it('should not pass validation if user\'s role is not found in authorization rules list', () => {
+  expect(authenticate({ user: 'User Name', role: 'VIEWER' }, { payload: { route: '/projects' } })).toEqual(false);
 });
