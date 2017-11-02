e1d1864582cd5d1ae6e79c2a99d4b6c20a93b016
miitri raatikainen <miitri.raatikainen@siili.com>
2017-11-02 11:47:38 +0200

    Attach click handler to a button element instead of list item in RoleList

--- src/components/RoleList/RoleList.js
+++ src/components/RoleList/RoleList.js
@@ -4,7 +4,6 @@ import { bindActionCreators } from 'redux';
 import * as R from 'ramda';
 import USER_ROLES from '../../constants/user_roles';
 import { actions } from '../../redux/users/users.ducks';
-import { filterEnter } from '../../utils';
 import './RoleList.css';
 
 /**
@@ -18,28 +17,20 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 }, dispatch);
 
 const RoleList = ({ selectUserRole }) => (
-  <div>
-    <ul className="FilesList clear-list-styles">
-      {R.toPairs(USER_ROLES).map(([role, user]) => (
-        <div key={role}>
-          <div className="three columns" />
-          <div className="six columns">
-            <li
-              role="button"
-              className="RoleListItem"
-              tabIndex="0"
-              onClick={() => selectUserRole({ role, user })}
-              onKeyPress={filterEnter(() => selectUserRole({ role, user }))}
-            >
-              <i className="fa fa-user fa-fw" aria-hidden="true" />&nbsp;
-              {user}
-            </li>
-          </div>
-          <div className="three columns" />
+  <ul className="FilesList clear-list-styles">
+    {R.toPairs(USER_ROLES).map(([role, user]) => (
+      <li key={role} className="RoleListItem">
+        <div className="three columns" />
+        <div className="six columns">
+          <button tabIndex="0" onClick={() => selectUserRole({ role, user })}>
+            <i className="fa fa-user fa-fw" aria-hidden="true" />&nbsp;
+            {user}
+          </button>
         </div>
-      ))}
-    </ul>
-  </div>
+        <div className="three columns" />
+      </li>
+    ))}
+  </ul>
 );
 
 export default connect(null, mapDispatchToProps)(RoleList);
