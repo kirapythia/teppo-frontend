fb1e2ee4f35231619babd78548949018abaf4da1
Toni Helminen <toni.helminen@unikie.com>
2018-09-28 14:51:49 +0300

    Added new function for listing comments grouped by names

--- src/components/PlanComments/PlanCommentsList.js
+++ src/components/PlanComments/PlanCommentsList.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import * as _ from 'lodash';
 import PlanCommentsListItem from './PlanCommentsListItem';
 import t from '../../locale';
 import './PlanCommentsList.css';
+import USER_ROLES from '../../constants/user_roles';
 
 const ListOfAllComments = (comments, toggleCommentApproval, readOnly) => (
   <ul className="PlanCommentsList clear-list-styles">
@@ -16,6 +18,29 @@ const ListOfAllComments = (comments, toggleCommentApproval, readOnly) => (
   </ul>
 );
 
+const GroupedListOfAllComments = (comments, toggleCommentApproval, readOnly) => {
+  const groupedComments = _.groupBy(comments, c => c.createdBy);
+  console.log(groupedComments);
+  return (
+    <div>
+      {Object.keys(groupedComments).map(commentGroup => (
+        <div key={commentGroup}>
+          <b>{commentGroup}</b>
+          <ul className="PlanCommentsList clear-list-styles">
+            {groupedComments[commentGroup].map(comment => (
+              <PlanCommentsListItem
+                key={comment.textId}
+                comment={comment}
+                readOnly={readOnly}
+                onApproveClick={toggleCommentApproval}
+              />
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+)};
+
 /**
  * A list component for dislaying a list of comments
  * @param {object} props
@@ -25,7 +50,8 @@ const ListOfAllComments = (comments, toggleCommentApproval, readOnly) => (
  */
 const PlanCommentsList = ({ comments = [], toggleCommentApproval, readOnly }) => (
   <div>
-    {!!comments.length && ListOfAllComments(comments, toggleCommentApproval, readOnly)}
+    {!!comments.length && GroupedListOfAllComments(comments, toggleCommentApproval, readOnly)}
+    {/*!!comments.length && ListOfAllComments(comments, toggleCommentApproval, readOnly)*/}
     {!comments.length && <div className="NoComments text-italic">{t('plan.comments.no_comments')}</div>}
   </div>
 );
