acb29c60c60961ebed1434b502357c12733760d6
Toni Helminen <toni.helminen@unikie.com>
2018-09-28 15:21:04 +0300

    Some refactoring to not to have duplicate code

--- src/components/PlanComments/PlanCommentsList.js
+++ src/components/PlanComments/PlanCommentsList.js
@@ -5,7 +5,7 @@ import t from '../../locale';
 import './PlanCommentsList.css';
 import USER_ROLES from '../../constants/user_roles';
 
-const ListOfAllComments = (comments, toggleCommentApproval, readOnly) => (
+const listOfComments = (comments, toggleCommentApproval, readOnly) => (
   <ul className="PlanCommentsList clear-list-styles">
     {comments.map(comment => (
       <PlanCommentsListItem
@@ -18,24 +18,14 @@ const ListOfAllComments = (comments, toggleCommentApproval, readOnly) => (
   </ul>
 );
 
-const GroupedListOfAllComments = (comments, toggleCommentApproval, readOnly) => {
+const groupedListOfAllComments = (comments, toggleCommentApproval, readOnly) => {
   const groupedComments = _.groupBy(comments, c => c.createdBy);
-  console.log(groupedComments);
   return (
     <div>
       {Object.keys(groupedComments).map(commentGroup => (
         <div key={commentGroup}>
           <b>{parseCategoryFromGroupName(commentGroup)}</b>
-          <ul className="PlanCommentsList clear-list-styles">
-            {groupedComments[commentGroup].map(comment => (
-              <PlanCommentsListItem
-                key={comment.textId}
-                comment={comment}
-                readOnly={readOnly}
-                onApproveClick={toggleCommentApproval}
-              />
-            ))}
-          </ul>
+          {listOfComments(groupedComments[commentGroup], toggleCommentApproval, readOnly)}
         </div>
       ))}
     </div>
@@ -54,8 +44,8 @@ const parseCategoryFromGroupName = (group) => {
  */
 const PlanCommentsList = ({ comments = [], toggleCommentApproval, readOnly }) => (
   <div>
-    {!!comments.length && GroupedListOfAllComments(comments, toggleCommentApproval, readOnly)}
-    {/*!!comments.length && ListOfAllComments(comments, toggleCommentApproval, readOnly)*/}
+    {!!comments.length && groupedListOfAllComments(comments, toggleCommentApproval, readOnly)}
+    {/*!!comments.length && listOfComments(comments, toggleCommentApproval, readOnly)*/}
     {!comments.length && <div className="NoComments text-italic">{t('plan.comments.no_comments')}</div>}
   </div>
 );
